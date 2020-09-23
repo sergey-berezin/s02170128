db46efa2faaec09578b0368f956234c310cda099
soapun <leha5617441@gmail.com>
2020-09-23 20:46:59 +0300

    uploading task_1

---
+++ ImageRecognition/PredictionQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageRecognition
+{
+    public class PredictionQueue
+    {
+            private readonly ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+            public event EventHandler Enqueued;
+            protected virtual void OnEnqueued(EventArgs e)
+            {
+                if (Enqueued != null)
+                    Enqueued(this, e);
+            }
+            public virtual void Enqueue(string item)
+            {
+                queue.Enqueue(item);
+                OnEnqueued(new EventArgs());
+            }
+            public virtual string TryDequeue()
+            {
+                string item; 
+                queue.TryDequeue(out item);
+                return item;
+            }
+    }
+}
---
+++ task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Threading.Tasks;
+using System.Drawing;
+using Xunit;
+using Xunit.Abstractions;
+using ImageRecognition;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace task_1
+{
+    class Program
+    {
+        static void PredictionCaught(object sender, EventArgs e)
+        {
+            Console.WriteLine((sender as PredictionQueue).TryDequeue());
+        }
+
+        static void Main(string[] args)
+        {
+            string ProjPath = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+            string DirPath = ProjPath + @"\pics_to_recognize\";
+
+            OnnxClassifier clf = new OnnxClassifier(ProjPath + @"\model\resnet50-v2-7.onnx");
+
+            PredictionQueue cq = new PredictionQueue();
+            cq.Enqueued += PredictionCaught;
+            clf.PredictAll(cq, DirPath);
+        }
+    }
+}
